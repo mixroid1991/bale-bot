ee1c447a913a49277481f4b1ed5945028b7f4193
Mixroid1991 <mixroid@gmail.com>
2025-07-10 05:09:13 +0330

    نسخه اولیه ربات

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os  # برای دریافت پورت از محیط اجرا
 
 app = Flask(__name__)
 
@@ -7,4 +8,6 @@ def home():
     return "سلام مازوت! رباتت روی Render زنده شد!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    # دریافت پورت از محیط اجرا؛ اگر نبود، پیش‌فرض 10000
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
